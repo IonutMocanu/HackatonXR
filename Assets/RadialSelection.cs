ff102fb91efb9c956862562cf2944cbaaebf82cc
IonutMocanu <ionutmocanu770@gmail.com>
2026-04-10 21:53:27 +0300

    Meniu circular controller

--- Assets/RadialSelection.cs
+++ Assets/RadialSelection.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using RangeAttribute = UnityEngine.RangeAttribute;
@@ -16,12 +16,20 @@ public class RadialSelection : MonoBehaviour
     public float AngleBetweenPart;
 
     private List<GameObject> m_spawnedParts = new();
+    private List<GameObject> m_spawnedIcons = new();
 
     public Transform HandTransform;
 
-    public UnityEvent<int> OnPartSelected;
-
     public OVRInput.Button SpawnButton;
+
+    [Header("Icons")]
+    public List<Sprite> RadialIcons;
+    public float IconDistance;
+
+    public float IconHoverDistance;
+
+    [Header("Events")]
+    public UnityEvent<int> OnPartSelected;
 
     private int m_currentSelectedPart = -1;
 
@@ -48,6 +56,8 @@ public class RadialSelection : MonoBehaviour
         {
             HideAndTiggerSelected();
         }
+        //CreateRadialUI();
+        //GetSelectedRadialPart();
     }
 
     public void HideAndTiggerSelected()
@@ -59,28 +69,46 @@ public class RadialSelection : MonoBehaviour
     public void GetSelectedRadialPart()
     {
         Vector3 centerToHand = HandTransform.position - RadialPartCanvas.position;
-        Vector3 centerToHandProjected = Vector3.ProjectOnPlane(centerToHand,RadialPartCanvas.forward);
+        Vector3 centerToHandProjected = Vector3.ProjectOnPlane(centerToHand, RadialPartCanvas.forward);
 
         float angle = Vector3.SignedAngle(RadialPartCanvas.up, centerToHandProjected, RadialPartCanvas.forward);
 
         if (angle < 0) angle += 360;
-        Debug.Log("Angle: " + angle.ToString());
 
-        m_currentSelectedPart = (int)angle * NumberOfRadialPart / 360;
+        m_currentSelectedPart = (int)(angle * NumberOfRadialPart / 360f);
 
-        Debug.Log("Index: " + m_currentSelectedPart.ToString());
+        float sliceAngle = 360f / NumberOfRadialPart;
+        float halfSliceAngle = sliceAngle / 2f;
 
         for (int i = 0; i < m_spawnedParts.Count; i++)
         {
-            if(i == m_currentSelectedPart)
+            Transform iconTransform = m_spawnedIcons[i].transform;
+            RectTransform iconRect = iconTransform.GetComponent<RectTransform>();
+
+            float iconGlobalAngle = (i * sliceAngle) + 90f - halfSliceAngle;
+            Vector3 direction2D = Quaternion.Euler(0, 0, iconGlobalAngle) * Vector3.up;
+
+            Vector3 defaultPos = direction2D * IconDistance;
+            defaultPos.z = -2f;
+
+            Vector3 hoverPos = direction2D * (IconDistance + IconHoverDistance);
+            hoverPos.z = -2f;
+
+            if (i == m_currentSelectedPart)
             {
                 m_spawnedParts[i].GetComponent<Image>().color = Color.yellow;
                 m_spawnedParts[i].transform.localScale = 1.1f * Vector3.one;
+
+                iconRect.localScale = 1.2f * Vector3.one;
+                iconRect.localPosition = hoverPos;
             }
             else
             {
                 m_spawnedParts[i].GetComponent<Image>().color = Color.white;
                 m_spawnedParts[i].transform.localScale = Vector3.one;
+
+                iconRect.localScale = Vector3.one;
+                iconRect.localPosition = defaultPos;
             }
         }
     }
@@ -91,28 +119,52 @@ public class RadialSelection : MonoBehaviour
         RadialPartCanvas.position = HandTransform.position;
         RadialPartCanvas.rotation = HandTransform.rotation;
 
-        foreach (var item in m_spawnedParts)
-        {
-            Destroy(item);
-        }
+        foreach (var item in m_spawnedParts) { Destroy(item); }
+        foreach (var item in m_spawnedIcons) { Destroy(item); }
 
         m_spawnedParts.Clear();
+        m_spawnedIcons.Clear();
+
+        float sliceAngle = 360f / NumberOfRadialPart;
+        float halfSliceAngle = sliceAngle / 2f;
 
         for (int i = 0; i < NumberOfRadialPart; i++)
         {
-            var angle = (i * 360 / NumberOfRadialPart - AngleBetweenPart / 2) + 90;
-
-            var radialPartEulerAngle = new Vector3(0, 0, angle);
+            float sliceStartAngle = (i * sliceAngle) + 90f - (AngleBetweenPart / 2f);
+            var radialPartEulerAngle = new Vector3(0, 0, sliceStartAngle);
 
             var spwanedRadialPart = Instantiate(RadialPartPrefab, RadialPartCanvas);
-
             spwanedRadialPart.transform.position = RadialPartCanvas.position;
-
             spwanedRadialPart.transform.localEulerAngles = radialPartEulerAngle;
-
             spwanedRadialPart.GetComponent<Image>().fillAmount = (1f / NumberOfRadialPart) - (AngleBetweenPart / 360f);
 
             m_spawnedParts.Add(spwanedRadialPart);
+
+            if (RadialIcons != null && i < RadialIcons.Count && RadialIcons[i] != null)
+            {
+                var icon = new GameObject(RadialIcons[i].name);
+                icon.transform.SetParent(RadialPartCanvas, false);
+                icon.transform.SetAsLastSibling();
+
+                icon.AddComponent<Image>().sprite = RadialIcons[i];
+                RectTransform iconRect = icon.GetComponent<RectTransform>();
+                iconRect.sizeDelta = 31f * Vector2.one;
+
+                float iconGlobalAngle = (i * sliceAngle) + 90f - halfSliceAngle;
+                Vector3 iconLocalPos = Quaternion.Euler(0, 0, iconGlobalAngle) * Vector3.up * IconDistance;
+
+                iconLocalPos.z = -2f;
+                iconRect.localPosition = iconLocalPos;
+                iconRect.localRotation = Quaternion.identity;
+
+                m_spawnedIcons.Add(icon);
+            }
+            else
+            {
+                var emptyIcon = new GameObject("EmptyIcon");
+                emptyIcon.transform.SetParent(RadialPartCanvas, false);
+                m_spawnedIcons.Add(emptyIcon);
+            }
         }
     }
 }
